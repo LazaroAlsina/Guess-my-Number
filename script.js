3d329b05b577b88b5f9d31944b4b8d761b2eb5b6
Lazaro <lazaroalsina@gmail.com>
2023-01-18 20:34:34 -0500

    setting up events

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 "use strict";
+
+/*
 console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "❤ Correct number!";
+
+document.querySelector(".number").textContent = 15;
+document.querySelector(".score").textContent = 10;
+
+console.log(document.querySelector(".guess").value);
+document.querySelector(".guess").value = 25;
+*/
