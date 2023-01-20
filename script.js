ee0e2cb6812cd300319c1ecc4316c08c377b0e17
Lazaro <lazaroalsina@gmail.com>
2023-01-19 23:57:15 -0500

    added a score counter that goes wrong everytime the guess is wrong

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ console.log(document.querySelector(".message").textContent);
 document.querySelector(".message").textContent = "❤ Correct number!";
 
 document.querySelector(".number").textContent = 15;
-document.querySelector(".score").textContent = 10;
 
 console.log(document.querySelector(".guess").value);
 document.querySelector(".guess").value = 25;
@@ -13,6 +12,8 @@ document.querySelector(".guess").value = 25;
 
 const guessNumber = Math.floor(Math.random() * 20 + 1);
 document.querySelector(".number").textContent = guessNumber;
+
+let score = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
   console.log(document.querySelector(".guess").value);
@@ -24,7 +25,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "Correct Number!";
   } else if (guessNumber < guess) {
     document.querySelector(".message").textContent = "Wrong number, Go lower!";
+    score -= 1;
+    document.querySelector(".score").textContent = score;
   } else if (guessNumber > guess) {
     document.querySelector(".message").textContent = "Wrong number, Go higher!";
+    score -= 1;
+    document.querySelector(".score").textContent = score;
   }
 });
